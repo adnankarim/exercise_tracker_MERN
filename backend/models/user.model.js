f4da4ac5843989932bc21cb2fdd7c3e0d57b85a9
adnankarim <skiwordylectures@gmail.com>
2020-08-03 23:44:33 +0500

    Updated Exercises Routes
    Added Exercises Routes for
    Delete
    Update
    Get Specific id Info

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const Schema = require("Schema");
+const Schema = mongoose.Schema;
 // trim removes spaces and timestamp: ? when it was CRUD
 const userSchema = new Schema(
   {
